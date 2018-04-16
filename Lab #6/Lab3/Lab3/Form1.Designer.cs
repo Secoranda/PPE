b65b7762d7fab619fd3b74c6f484c60ebc51f97c
Secoranda <cojocari.daniela.2@gmail.com>
2018-04-16 15:30:28 +0300

    Add files via upload

---
+++ Lab #6/Lab3/Lab3/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace PPE_Lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Canvas = new System.Windows.Forms.PictureBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.width = new System.Windows.Forms.ToolStripDropDownButton();
+            this.widthThin = new System.Windows.Forms.ToolStripMenuItem();
+            this.widthMedium = new System.Windows.Forms.ToolStripMenuItem();
+            this.widthThick = new System.Windows.Forms.ToolStripMenuItem();
+            this.shapes = new System.Windows.Forms.ToolStripDropDownButton();
+            this.line = new System.Windows.Forms.ToolStripMenuItem();
+            this.square = new System.Windows.Forms.ToolStripMenuItem();
+            this.circle = new System.Windows.Forms.ToolStripMenuItem();
+            this.triangle = new System.Windows.Forms.ToolStripMenuItem();
+            this.style = new System.Windows.Forms.ToolStripDropDownButton();
+            this.filled = new System.Windows.Forms.ToolStripMenuItem();
+            this.unfilled = new System.Windows.Forms.ToolStripMenuItem();
+            this.color = new System.Windows.Forms.ToolStripMenuItem();
+            this.clear = new System.Windows.Forms.ToolStripButton();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.lines = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.Canvas)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+
+            this.Canvas.BackColor = System.Drawing.Color.White;
+            this.Canvas.Location = new System.Drawing.Point(12, 40);
+            this.Canvas.Name = "Canvas";
+            this.Canvas.Size = new System.Drawing.Size(683, 316);
+            this.Canvas.TabIndex = 0;
+            this.Canvas.TabStop = false;
+            this.Canvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseDown);
+            this.Canvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseMove);
+            this.Canvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseUp);
+
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.width,
+            this.shapes,
+            this.style,
+            this.clear});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(707, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+
+            this.width.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.width.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.widthThin,
+            this.widthMedium,
+            this.widthThick,
+            });
+            this.width.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.width.Name = "width";
+           this.width.Size = new System.Drawing.Size(75, 22);
+            this.width.Text = "Width";
+
+            this.widthThin.Name = "widthThin";
+            this.widthThin.Size = new System.Drawing.Size(75, 22);
+            this.widthThin.Text = "Thin";
+            this.widthThin.Click += new System.EventHandler(this.widthThin_Click);
+
+            this.widthMedium.Name = "widthMedium";
+            this.widthMedium.Size = new System.Drawing.Size(75, 22);
+            this.widthMedium.Text = "Medium";
+            this.widthMedium.Click += new System.EventHandler(this.widthMedium_Click);
+
+            this.widthThick.Name = "widthThick";
+            this.widthThick.Size = new System.Drawing.Size(75, 22);
+            this.widthThick.Text = "Thick";
+            this.widthThick.Click += new System.EventHandler(this.widthThick_Click);
+
+            this.shapes.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.shapes.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.line,
+            this.square,
+            this.circle,
+            this.triangle,
+            this.lines});
+            this.shapes.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.shapes.Name = "shapes";
+            this.shapes.Size = new System.Drawing.Size(75, 22);
+            this.shapes.Text = "Shapes";
+
+            this.line.Name = "line";
+            this.line.Size = new System.Drawing.Size(75, 22);
+            this.line.Text = "Paint";
+            this.line.Click += new System.EventHandler(this.line_Click);
+
+            this.square.Name = "square";
+            this.square.Size = new System.Drawing.Size(75, 22);
+            this.square.Text = "Rectangle";
+            this.square.Click += new System.EventHandler(this.square_Click);
+
+            this.circle.Name = "circle";
+            this.circle.Size = new System.Drawing.Size(75, 22);
+            this.circle.Text = "Circle";
+            this.circle.Click += new System.EventHandler(this.circle_CLICK);
+
+            this.triangle.Name = "triangle";
+            this.triangle.Size = new System.Drawing.Size(75, 22);
+            this.triangle.Text = "Triangle";
+            this.triangle.Click += new System.EventHandler(this.triangle_Click);
+
+            this.style.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.style.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.filled,
+            this.unfilled,
+            this.color});
+            this.style.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.style.Name = "style";
+            this.style.Size = new System.Drawing.Size(75, 22);
+            this.style.Text = "Change style";
+
+            this.filled.Name = "filled";
+            this.filled.Size = new System.Drawing.Size(75, 22);
+            this.filled.Text = "Filled";
+            this.filled.Click += new System.EventHandler(this.filled_CLICK);
+
+            this.unfilled.Name = "unfilled";
+            this.unfilled.Size = new System.Drawing.Size(75, 22);
+            this.unfilled.Text = "Unfilled";
+            this.unfilled.Click += new System.EventHandler(this.unfilled_Click);
+
+            this.color.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.color.Name = "toolStripColorButton";
+            this.color.Size = new System.Drawing.Size(75, 22);
+            this.color.Text = "Colors";
+            this.color.Click += new System.EventHandler(this.color_Click);
+
+            this.clear.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.clear.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(75, 22);
+            this.clear.Text = "Clear";
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+
+            this.lines.Name = "lines";
+            this.lines.Size = new System.Drawing.Size(75, 22);
+            this.lines.Text = "Lines";
+            this.lines.Click += new System.EventHandler(this.lines_Click);
+
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(707, 368);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.Canvas);
+            this.MinimumSize = new System.Drawing.Size(400, 200);
+            this.Name = "Form1";
+            this.Text = "LAB6";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.Canvas)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Canvas;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton width;
+        private System.Windows.Forms.ToolStripMenuItem widthThin;
+        private System.Windows.Forms.ToolStripMenuItem widthMedium;
+        private System.Windows.Forms.ToolStripMenuItem widthThick;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ToolStripButton clear;
+        private System.Windows.Forms.ToolStripDropDownButton shapes;
+        private System.Windows.Forms.ToolStripMenuItem line;
+        private System.Windows.Forms.ToolStripMenuItem square;
+        private System.Windows.Forms.ToolStripMenuItem circle;
+        private System.Windows.Forms.ToolStripMenuItem triangle;
+        private System.Windows.Forms.ToolStripDropDownButton style;
+        private System.Windows.Forms.ToolStripMenuItem filled;
+        private System.Windows.Forms.ToolStripMenuItem unfilled;
+        private System.Windows.Forms.ToolStripMenuItem color;
+        private System.Windows.Forms.ToolStripMenuItem lines;
+    }
+}
+
---
+++ Lab #6/Lab3/Lab3/Form1.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PPE_Lab3
+{
+    public partial class Form1 : Form
+    {
+
+        private bool paint;
+        private bool drawFilledObjects;
+
+        private Point beginP;
+        private Point endP;
+
+        private Pen pencil;
+        private Pen redraw_pencil;
+
+        private SolidBrush brush;
+        private SolidBrush refill_brush;
+
+        private float pencil_offset;
+        private Graphics g;
+
+        private string draw_OBJ;
+
+        private Point initialP;
+        private int direction;
+
+        public Form1()
+        {
+            InitializeComponent();
+            pencil_offset = 1.4f;
+            paint = false;
+            drawFilledObjects = false;
+
+
+            draw_OBJ = "Line";
+
+            beginP = new Point(0, 0);
+            endP = new Point(0, 0);
+
+            g = Canvas.CreateGraphics();
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            brush = new SolidBrush(Color.Black);
+            refill_brush = new SolidBrush(Color.White);
+
+            pencil = new Pen(Color.Black, 1);
+            pencil.StartCap = pencil.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+
+            redraw_pencil = new Pen(Color.White, 1 + pencil_offset);
+            redraw_pencil.StartCap = redraw_pencil.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+
+            initialP = new Point();
+            direction = 1;
+        }
+
+        private void Canvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            paint = false;
+        }
+
+        private void Canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            paint = true;
+            beginP.X = e.X;
+            beginP.Y = e.Y;
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (paint)
+            {
+                switch (draw_OBJ)
+                {
+                    case "Line":
+                        DrawLine(e);
+                        break;
+                    case "Lines":
+                        DrawLines(e);
+                        break;
+                    case "Rectangle":
+                        DrawRectangle(e);
+                        break;
+                    case "Ellipse":
+                        DrawEllipse(e);
+                        break;
+                    case "Triangle":
+                        DrawTriangle(e);
+                        break;
+                    default:
+                        MessageBox.Show("Mouse not pressed");
+                        break;
+                }
+            }
+            else
+            {
+                beginP.X = endP.X = e.X;
+                beginP.Y = endP.Y = e.Y;
+                initialP.X = e.X;
+                initialP.Y = e.Y;
+                direction = 1;
+            }
+        }
+        private void DrawLine(MouseEventArgs e)
+        {
+            endP = new Point(e.X, e.Y);
+            g.DrawLine(pencil, beginP, endP);
+  
+            beginP = endP;
+        }
+
+        private void DrawLines(MouseEventArgs e)
+        {
+            g.DrawLine(redraw_pencil, beginP, endP);
+            endP = new Point(e.X, e.Y);
+            g.DrawLine(pencil, beginP, endP);
+        }
+
+        private void DrawTriangle(MouseEventArgs e)
+        {
+            if (drawFilledObjects)
+            {
+                int offset = 0;
+                if (beginP.X < endP.X)
+                    offset = 2;
+                else
+                    offset = -2;
+
+                Point[] points =
+                {
+                    new Point(beginP.X-offset, beginP.Y+Math.Abs(offset/2)*direction ),
+                    new Point(endP.X+offset, beginP.Y+Math.Abs(offset/2)*direction),
+                    new Point((endP.X + beginP.X) / 2 , beginP.Y - (Math.Abs(endP.X - beginP.X) / 2 + Math.Abs(offset/2))*direction)
+
+                };
+
+                g.FillPolygon(refill_brush, points);
+
+                if (initialP.Y >= e.Y)
+                    direction = 1;
+                else
+                    direction = -1;
+
+                endP.X = e.X;
+                endP.Y = e.Y;
+
+                Point[] points2 =
+                {
+                    new Point(beginP.X, beginP.Y),
+                    new Point(endP.X, beginP.Y),
+                    new Point((endP.X + beginP.X) / 2, beginP.Y - (Math.Abs(endP.X - beginP.X) / 2)*direction)
+                };
+
+                g.FillPolygon(brush, points2);
+            }
+            else
+            {
+                Point[] points =
+                {
+                    new Point(beginP.X, beginP.Y),
+                    new Point(endP.X, beginP.Y),
+                    new Point((endP.X + beginP.X) / 2, beginP.Y - (Math.Abs(endP.X - beginP.X) / 2*direction ))
+
+                };
+
+                g.DrawPolygon(redraw_pencil, points);
+
+                if (initialP.Y >= e.Y)
+                    direction = 1;
+                else
+                    direction = -1;
+
+                endP.X = e.X;
+                endP.Y = e.Y;
+                Point[] points2 =
+                {
+                    new Point(beginP.X, beginP.Y),
+                    new Point(endP.X, beginP.Y),
+                    new Point((endP.X + beginP.X) / 2, beginP.Y - (Math.Abs(endP.X - beginP.X) / 2*direction))
+                };
+                g.DrawPolygon(pencil, points2);
+            }
+        }
+
+        private void DrawRectangle(MouseEventArgs e)
+        {
+
+            if (drawFilledObjects)
+            {
+                g.FillRectangle(refill_brush, beginP.X - pencil_offset / 2, beginP.Y - pencil_offset / 2,
+                    Math.Abs(endP.X - beginP.X) + pencil_offset, Math.Abs(endP.Y - beginP.Y) + pencil_offset);
+
+                if (e.X >= initialP.X)
+                    endP.X = e.X;
+                else
+                    beginP.X = e.X;
+
+                if (e.Y >= initialP.Y)
+                    endP.Y = e.Y;
+                else
+                    beginP.Y = e.Y;
+
+                g.FillRectangle(brush, beginP.X, beginP.Y,
+                    Math.Abs(endP.X - beginP.X), Math.Abs(endP.Y - beginP.Y));
+            }
+            else
+            {
+                g.DrawRectangle(redraw_pencil, beginP.X, beginP.Y,
+                    Math.Abs(endP.X - beginP.X), Math.Abs(endP.Y - beginP.Y));
+
+                if (e.X >= initialP.X)
+                    endP.X = e.X;
+                else
+                    beginP.X = e.X;
+
+                if (e.Y >= initialP.Y)
+                    endP.Y = e.Y;
+                else
+                    beginP.Y = e.Y;
+
+                g.DrawRectangle(pencil, beginP.X, beginP.Y,
+                    Math.Abs(endP.X - beginP.X), Math.Abs(endP.Y - beginP.Y));
+            }
+
+        }
+
+        private void DrawEllipse(MouseEventArgs e)
+        {
+            if (drawFilledObjects)
+            {
+                float redrawOffset = 2.4f;
+                g.FillEllipse(refill_brush, beginP.X - redrawOffset / 2, beginP.Y - redrawOffset / 2,
+                    Math.Abs(endP.X - beginP.X) + redrawOffset, Math.Abs(endP.Y - beginP.Y) + redrawOffset);
+
+                if (e.X >= initialP.X)
+                    endP.X = e.X;
+                else
+                    beginP.X = e.X;
+
+                if (e.Y >= initialP.Y)
+                    endP.Y = e.Y;
+                else
+                    beginP.Y = e.Y;
+
+                g.FillEllipse(brush, beginP.X, beginP.Y,
+                    Math.Abs(endP.X - beginP.X), Math.Abs(endP.Y - beginP.Y));
+
+            }
+            else
+            {
+
+                g.DrawEllipse(redraw_pencil, beginP.X, beginP.Y,
+                    Math.Abs(endP.X - beginP.X), Math.Abs(endP.Y - beginP.Y));
+
+                if (e.X >= initialP.X)
+                    endP.X = e.X;
+                else
+                    beginP.X = e.X;
+
+                if (e.Y >= initialP.Y)
+                    endP.Y = e.Y;
+                else
+                    beginP.Y = e.Y;
+
+                g.DrawEllipse(pencil, beginP.X, beginP.Y,
+                    Math.Abs(endP.X - beginP.X), Math.Abs(endP.Y - beginP.Y));
+            }
+        }
+
+        private void color_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+            if (cd.ShowDialog() == DialogResult.OK)
+            {
+                pencil.Color = cd.Color;
+                brush.Color = cd.Color;
+            }
+        }
+
+        private void widthThin_Click(object sender, EventArgs e)
+        {
+            pencil.Width = 1;
+            redraw_pencil.Width = 1 + pencil_offset;
+        }
+
+        private void widthMedium_Click(object sender, EventArgs e)
+        {
+            pencil.Width = 10;
+            redraw_pencil.Width = 5 + pencil_offset;
+        }
+
+        private void widthThick_Click(object sender, EventArgs e)
+        {
+            pencil.Width = 20;
+            redraw_pencil.Width = 10 + pencil_offset;
+        }
+
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+        }
+
+        private void line_Click(object sender, EventArgs e)
+        {
+            draw_OBJ = "Line";
+
+        }
+
+        private void square_Click(object sender, EventArgs e)
+        {
+            draw_OBJ = "Rectangle";
+        }
+
+        private void circle_CLICK(object sender, EventArgs e)
+        {
+            draw_OBJ = "Ellipse";
+        }
+
+        private void triangle_Click(object sender, EventArgs e)
+        {
+            draw_OBJ = "Triangle";
+        }
+
+        private void filled_CLICK(object sender, EventArgs e)
+        {
+            drawFilledObjects = true;
+        }
+
+        private void unfilled_Click(object sender, EventArgs e)
+        {
+            drawFilledObjects = false;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            this.Canvas.Width = this.Width - 40;
+            this.Canvas.Height = this.Height - 90;
+
+            g = Canvas.CreateGraphics();
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+        }
+
+        private void lines_Click(object sender, EventArgs e)
+        {
+            draw_OBJ = "Lines";
+        }
+    }
+}
